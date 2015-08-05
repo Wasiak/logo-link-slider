540ebdfb68e9193f712eca132911d030dd7aee1a
Michał Budzyński <michal@virtualdesign.pl>
2015-08-05 01:01:00 +0100

    :older_man: change in mainjs

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
 var slider = document.getElementById('slider');
-
-var Logo = function(item) {
-  var logoBox = document.createElement('a');
-  slider.appendChild(logoBox);
-  logoBox.href = item.link;
-
-  var logo = document.createElement('img');
-  logoBox.appendChild(logo);
-  logo.id = item.name;
-  logo.src = item.image;
-};
-
-var items = [
-  {'name' : 'name1', 'image' : 'images/01.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name2', 'image' : 'images/02.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name3', 'image' : 'images/03.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name4', 'image' : 'images/04.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name5', 'image' : 'images/05.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name6', 'image' : 'images/06.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name7', 'image' : 'images/07.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name8', 'image' : 'images/08.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name9', 'image' : 'images/09.png', 'link' : 'http://www.legionisci.com'},
-  {'name' : 'name10', 'image' : 'images/10.png', 'link' : 'http://www.legionisci.com'}
-];
-
-var initLogos = function() {
-  for (i = 0; i < 4; i++) {
-    new Logo(items[i]);
-  }
-}();
-
 var nextToChange = 0;
 var nextToAdd = 4;
 var changeLogo = function() {
